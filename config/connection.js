0389954f6ea62fd904c59eb2e892411801a4e594
Aks Sreenivasan <aks.auburn@gmail.com>
2021-04-26 23:00:14 +1000

    fixed connection file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 var mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    // NOTE: Be sure to add your MySQL password here!
-    password: 'candy',
-    database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        // NOTE: Be sure to add your MySQL password here!
+        password: 'candy',
+        database: 'burgers_db',
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
